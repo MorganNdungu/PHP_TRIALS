95b7da6007ff5f3f8e1aead731cc58f743518e28
morgan <126456427+MorganNdungu@users.noreply.github.com>
2023-08-27 20:08:49 +0300

    Add files via upload

---
+++ pro_ject/employee.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HTML Table with 5 Columns</title>
+</head>
+<body>
+
+<table border="1">
+    <tr>
+        <th>id</th>
+        <th>first_name </th>
+        <th>last_name</th>
+        <th>email</th>
+        <th>phone</th>
+    </tr>
+    <tr>
+        <td>x</td>
+        <td>y</td>
+        <td>z</td>
+        <td>a</a></td>
+        <td>b</td>
+    </tr>
+    
+</table>
+
+</body>
+</html>
